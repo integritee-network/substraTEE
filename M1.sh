8014ea46882be82f782fc7f8669f70eec2731be2
Marcel Frei <marcel.frei@electronix.ch>
2019-05-15 07:40:15 +0200

    Update for M1

--- M1.sh
+++ M1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# execute commands for M1
+
 # command to start the docker container
 # docker run -v $(pwd):/substraTEE/backup -ti substratee
 
 echo "start the node in the backgroud"
 /substraTEE/substraTEE-node-M1/target/release/substratee-node --dev > /substraTEE/node.log 2>&1 &
+sleep 1
 
 echo ""
 echo "start the worker to generate the keys"
@@ -15,10 +18,12 @@ cd /substraTEE/substraTEE-worker-M1
 echo ""
 echo "Start the worker in the background"
 ./bin/substratee_worker worker > /substraTEE/worker.log 2>&1 &
+sleep 1
 
 echo ""
 echo "Start the client"
 ./bin/substratee_client | tee /substraTEE/client.log
+sleep 1
 
 echo ""
 echo "Query the counter"
