03d19e061f23ead52d58063a28143dfe67ce7d72
Marcel Frei <marcel.frei@electronix.ch>
2019-07-18 16:25:01 +0200

    delay client a little bit more

--- scriptsM4/start_client.sh
+++ scriptsM4/start_client.sh
@@ -10,14 +10,14 @@ cd /substraTEE/substraTEE-worker-M4/bin
 ./substratee_client -p 9977 -s 192.168.10.10 2>&1 | tee /substraTEE/output/client_first.log
 
 # wait until worker 2 registered
-sleep 10s
+sleep 30s
 
 # start the client and send second transaction
 cd /substraTEE/substraTEE-worker-M4/bin
 ./substratee_client -p 9977 -s 192.168.10.10 2>&1 | tee /substraTEE/output/client_second.log
 
 # wait until transaction is processed
-sleep 20s
+sleep 30s
 
 # query the counter
 cd /substraTEE/substraTEE-worker-M4/bin
