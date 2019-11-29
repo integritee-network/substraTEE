6b5e3888dff17bb8f2b0bd69f98b20f861d6038a
Marcel Frei <marcel.frei@electronix.ch>
2019-11-29 14:51:25 +0100

    start_client.sh: only perform client (incognito TX) once

--- scriptsM5/start_client.sh
+++ scriptsM5/start_client.sh
@@ -2,25 +2,11 @@
 
 clear
 
-# wait until the worker 1 is ready
+# wait until the worker is ready
 sleep 32s
 
-# start the client and send first transaction
+# start the client and perform an incognito transfer
 cd /substraTEE/substraTEE-worker/bin
-./substratee_client -p 9977 -a 192.168.10.10 2>&1 | tee /substraTEE/output/client_first.log
-
-# wait until worker 2 registered
-sleep 30s
-
-# start the client and send second transaction
-cd /substraTEE/substraTEE-worker/bin
-./substratee_client -p 9977 -a 192.168.10.10 2>&1 | tee /substraTEE/output/client_second.log
-
-# wait until transaction is processed
-sleep 30s
-
-# start the client and send third transaction
-cd /substraTEE/substraTEE-worker/bin
-./substratee_client -p 9977 -a 192.168.10.10 2>&1 | tee /substraTEE/output/client_second.log
+./substratee_client -p 9977 -a 192.168.10.10 2>&1 | tee /substraTEE/output/client.log
 
 read -p "Press enter to continue"
